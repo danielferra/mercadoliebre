86fb9f3ecde95a92b407bc9899f7418b74dde16f
danielferra <dferraioli@homtmail.com>
2021-11-07 18:29:25 -0300

    partial commit

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const app = express();
 const path = require('path')
-const port =3030
-app.use(express.static(__dirname + '/public'));
+const port = 3030
+const publicFolderPath = path.resolve(__dirname, "./public");
+app.use(express.static(publicFolderPath));
 
-app.get("/", (res, resp)=> resp.sendFile(path.join(__dirname,"/views/home.html")))
+app.get("/", (res, resp) => resp.sendFile(path.join(__dirname, "/views/home.html")))
 
-app.listen(port,()=>{console.log("servidor activo")})
+app.listen(port, () => { console.log("servidor activo") })
 
 
- 
